1cf5a7bed66a9285b3d372d19d36c88efa75e8a6
Daniel Ujikawa <danielujikawa@gmail.com>
2018-12-04 12:28:30 -0200

    controllers-v1

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,20 @@
+class BookingsController < ApplicationController
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def create
+    @offer = Offer.find(params[:offer_id])
+    @booking = Booking.new
+    @booking.offer = @offer
+    @booking.user = User.first
+    @booking.save!
+      # if current_user
+      #   redirect_to booking_show_path
+      # else
+      #   redirect_to user_session
+      # end
+    end
+
+end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,10 +2,12 @@ class Offer < ApplicationRecord
 
   belongs_to :user
 
+  validates :delivery_date, presence: true
   validates :origin, presence: true
   validates :destination, presence: true
   validates :size, presence: true
   validates :image, presence: true
   validates :price, presence: true
+  validates :comment, presence: true
 
 end
