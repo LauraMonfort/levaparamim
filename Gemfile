9f7dcdeb147ea93423ddd24c76cef458cdfef9c0
laura mft <lauramonfort@live.fr>
2018-12-04 18:01:32 -0200

    Gem config

--- Gemfile
+++ Gemfile
@@ -16,6 +16,9 @@ gem 'simple_form'
 gem 'uglifier'
 gem 'webpacker'
 
+# Messenger-bot gem
+gem 'messenger-ruby'
+
 group :development do
   gem 'web-console', '>= 3.3.0'
 end
---
+++ config/initializers/messenger.rb
@@ -0,0 +1,5 @@
+# YOUR_APP/config/initializers/messenger.rb
+Messenger.configure do |config|
+  config.verify_token      = '268827d8bdbf5993d0f545cdf1127818' #will be used in webhook verifiction
+  config.page_access_token = 'EAAFdjDf01VkBAImcYHDkXnI1rIxBHVChR10CZBowQosl61uSSUv2E5LD6v3KIAZAp2uwzUJUDVRiqnGnPZAd9cNAzIpyuaHsL6v0MN0N3sZBtZCxV66i8KX22jZBQw0Um02I8RBV69kYLMIaMkHGMZCQuqBVzJZANVj6bDLtisxuZBAZDZD'
+end
