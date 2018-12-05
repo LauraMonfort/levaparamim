f7a4d986c7f1a4b4e498e11e7db7e03631774646
Bruno Toledo <btoledo1973@gmail.com>
2018-12-05 19:54:23 -0200

    form no navbar

--- db/schema.rb
+++ db/schema.rb
@@ -35,11 +35,8 @@ ActiveRecord::Schema.define(version: 2018_12_05_183335) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
-    t.boolean "booked", default: false
-
     t.string "image"
-
+    t.boolean "booked", default: false
     t.index ["user_id"], name: "index_requests_on_user_id"
   end
 
