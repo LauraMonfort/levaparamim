e88f17db923f5757567d0e8855509e47f18a72d6
laura mft <lauramonfort@live.fr>
2018-12-05 17:25:42 -0200

    Fixed indentation on bot

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -2,11 +2,11 @@ require 'facebook/messenger'
 include Facebook::Messenger
 
 Bot.on :message do |message|
-    if message.text.include? "Bonjour"
-       message.reply(text: "Wesh maggl")
-   elsif message.text.include? "Bonne nuit"
-        message.reply(text: "Dors bien fdp")
-    else
-        message.reply(text: 'Reponse par defaut')
-
+  if message.text.include? "Bonjour"
+    message.reply(text: "Wesh maggl")
+  elsif message.text.include? "Bonne nuit"
+    message.reply(text: "Dors bien fdp")
+  else
+    message.reply(text: 'Reponse par defaut')
+  end
 end
