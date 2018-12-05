cda38da50e68b39e3d31e8d5c2ed47bf82f30648
laura mft <lauramonfort@live.fr>
2018-12-05 17:06:48 -0200

    test v2 pushing on heroku

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -2,5 +2,11 @@ require 'facebook/messenger'
 include Facebook::Messenger
 
 Bot.on :message do |message|
-  message.reply(text: 'Hello, human!')
+    if message.text.include? "Bonjour"
+       message.reply(text: "Wesh maggl")
+   elsif message.text.include? "Bonne nuit"
+        message.reply(text: "Dors bien fdp")
+    else
+        message.reply(text: 'Reponse par defaut')
+
 end
