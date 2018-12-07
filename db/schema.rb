1d210d6471df34eb5b6c2bece97f983c8156112d
Daniel Ujikawa <danielujikawa@gmail.com>
2018-12-07 15:17:12 -0200

    novo seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_07_142232) do
+ActiveRecord::Schema.define(version: 2018_12_07_165916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,9 @@ ActiveRecord::Schema.define(version: 2018_12_07_142232) do
     t.bigint "booking_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["booking_id"], name: "index_reviews_on_booking_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -70,4 +72,5 @@ ActiveRecord::Schema.define(version: 2018_12_07_142232) do
   add_foreign_key "bookings", "users"
   add_foreign_key "requests", "users"
   add_foreign_key "reviews", "bookings"
+  add_foreign_key "reviews", "users"
 end
