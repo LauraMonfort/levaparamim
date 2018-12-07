43af63c8594e2cec70ae8040a4f4524b7d2d4b7c
laura mft <lauramonfort@live.fr>
2018-12-07 13:08:17 -0200

    Autocomplete search bar done

---
+++ config/initializers/algoliasearch.rb
@@ -0,0 +1,14 @@
+YourAPIKey = 'b63adfa700d6e954f1994e8adee1b317'
+YourApplicationID = 'plWOSKD9GI9P'
+AlgoliaSearch.configuration = { application_id: 'YourApplicationID', api_key: 'YourAPIKey' }
+
+
+AlgoliaSearch.configuration = {
+  application_id: 'YourApplicationID',
+  api_key: 'YourAPIKey',
+  connect_timeout: 2,
+  receive_timeout: 30,
+  send_timeout: 30,
+  batch_timeout: 120,
+  search_timeout: 5
+}
